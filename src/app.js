95b41b502f61002636989d18b4152fce2cd607a7
Thabiso Motlhomi <thabisomotlhomi@Thabisos-MacBook-Pro.local>
2026-06-13 20:31:43 +0200

    Implement ESLint and Prettier

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.status(200).send("Hello from Acquistions Service!");
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from Acquisitions Service!');
 });
 
 export default app;
